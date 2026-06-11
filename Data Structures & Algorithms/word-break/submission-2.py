82ada957c2c5eac6aa94ac60b99c6d635c3a35dd
24f3002284 <24f3002284@ds.study.iitm.ac.in>
2026-06-11 12:13:07 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n=len(s)
+        dp=[False for i in range(n+1)] # indices of dp can be from 0,1,2,3..n-1,n
+
+        dp[n]=True # if we reach the end of s, the idx after n-1, => successful=>T
+        for i in range(n-1,-1,-1):
+            for w in wordDict:
+                if dp[i]==True:
+                    break
+                if i+len(w)-1<n and s[i:i+len(w)]==w:
+                    dp[i]=dp[i+len(w)]
+                
+
+        return dp[0]
